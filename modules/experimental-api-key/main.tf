17faee8fc7c385bcd17bd9587962896dc48ab745
shemau <shemau@gmail.com>
2024-10-31 18:14:24 +0000

    fix: only source file if it exists

--- modules/experimental-api-key/main.tf
+++ modules/experimental-api-key/main.tf
@@ -20,6 +20,7 @@ resource "terraform_data" "bootstrap_api_key" {
 
 # Sensitively read output
 data "local_sensitive_file" "bootstrap_api_key" {
+  count    = fileexists("bootstrap_api_key.txt") ? 1 : 0
   filename = "bootstrap_api_key.txt"
 
   depends_on = [resource.terraform_data.bootstrap_api_key]
@@ -27,7 +28,7 @@ data "local_sensitive_file" "bootstrap_api_key" {
 
 # Find values in JSON, retaining sensitive on api_key
 locals {
-  api_key      = regexall("(.*api_key\":\")([0-9a-zA-Z_]*)", data.local_sensitive_file.bootstrap_api_key.content)[0][1]
-  api_key_id   = nonsensitive(regexall("(.*api_key_id\":\")([0-9a-fApiKey-]*)", data.local_sensitive_file.bootstrap_api_key.content)[0][1])
-  api_key_name = nonsensitive(regexall("(.*api_key_name\":\")([0-9a-zA-Z-_]*)", data.local_sensitive_file.bootstrap_api_key.content)[0][1])
+  api_key      = regexall("(.*api_key\":\")([0-9a-zA-Z_]*)", data.local_sensitive_file.bootstrap_api_key[0].content)[0][1]
+  api_key_id   = nonsensitive(regexall("(.*api_key_id\":\")([0-9a-fApiKey-]*)", data.local_sensitive_file.bootstrap_api_key[0].content)[0][1])
+  api_key_name = nonsensitive(regexall("(.*api_key_name\":\")([0-9a-zA-Z-_]*)", data.local_sensitive_file.bootstrap_api_key[0].content)[0][1])
 }
