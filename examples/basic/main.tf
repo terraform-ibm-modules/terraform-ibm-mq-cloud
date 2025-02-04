97e653d25b5dfbf2d0af8640cf3afbae1ebdc60a
shemau <shemau@gmail.com>
2025-02-04 16:42:07 +0000

    fix: avoid coding version

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -33,7 +33,7 @@ module "mq_on_cloud" {
   queue_manager_display_name = "${var.prefix}-qm-display"
   queue_manager_name         = "${local.prefix}_qm"
   queue_manager_size         = "xsmall"
-  queue_manager_version      = "9.4.0_3"
+  queue_manager_version      = "9.4.1_2" # Pinned to current version
 
   applications = {
     "app-${local.prefix}" = {
