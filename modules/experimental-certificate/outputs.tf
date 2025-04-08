ac853e6d6abc62b00046b5bc202aa413771b3039
shemau <shemau@gmail.com>
2025-04-08 13:23:11 +0100

    fix: improve ffdc

--- modules/experimental-certificate/outputs.tf
+++ modules/experimental-certificate/outputs.tf
@@ -3,7 +3,12 @@
 ########################################################################################################################
 
 output "certificate" {
-  description = "name"
+  description = "The value of the requested certificate"
   value       = data.external.certificate.result.certificate
   sensitive   = true
+}
+
+output "errors" {
+  description = "Diagnostics of any errors reported by during the lookup process"
+  value       = data.external.certificate.result.errors
 }
--- modules/experimental-connection/outputs.tf
+++ modules/experimental-connection/outputs.tf
@@ -3,61 +3,66 @@
 ########################################################################################################################
 
 output "connection_admin_channel_name" {
-  description = "name"
+  description = "Channel name of the admin channel"
   value       = data.external.connection.result.channel_0_name
 }
 
 output "connection_admin_host" {
-  description = "name"
+  description = "Host name of the admin channel"
   value       = data.external.connection.result.channel_0_clientConnection_connection_0_host
 }
 
 output "connection_admin_port" {
-  description = "name"
+  description = "Port of of the admin channel"
   value       = data.external.connection.result.channel_0_clientConnection_connection_0_port
 }
 
 output "connection_admin_queue_manager_name" {
-  description = "name"
+  description = "Queue manager name of the admin channel"
   value       = data.external.connection.result.channel_0_clientConnection_queueManager
 }
 
 output "connection_admin_cipherspec" {
-  description = "name"
+  description = "Cipher specification of the admin channel"
   value       = data.external.connection.result.channel_0_transmissionSecurity_cipherSpecification
 }
 
 output "connection_admin_type" {
-  description = "name"
+  description = "Connection type of the admin channel"
   value       = data.external.connection.result.channel_0_type
 }
 
 output "connection_app_channel_name" {
-  description = "name"
+  description = "Channel name of the application channel"
   value       = data.external.connection.result.channel_1_name
 }
 
 output "connection_app_host" {
-  description = "name"
+  description = "Host name of the application channel"
   value       = data.external.connection.result.channel_1_clientConnection_connection_0_host
 }
 
 output "connection_app_port" {
-  description = "name"
+  description = "Port of the application channel"
   value       = data.external.connection.result.channel_1_clientConnection_connection_0_port
 }
 
 output "connection_app_queue_manager_name" {
-  description = "name"
+  description = "Queue manager name of the application channel"
   value       = data.external.connection.result.channel_1_clientConnection_queueManager
 }
 
 output "connection_app_cipherspec" {
-  description = "name"
+  description = "Cipher specification of the application channel"
   value       = data.external.connection.result.channel_1_transmissionSecurity_cipherSpecification
 }
 
 output "connection_app_type" {
-  description = "name"
+  description = "Connection type of the application channel"
   value       = data.external.connection.result.channel_1_type
+}
+
+output "errors" {
+  description = "Diagnostics of any errors reported by during the lookup process"
+  value       = data.external.connection.result.errors
 }
