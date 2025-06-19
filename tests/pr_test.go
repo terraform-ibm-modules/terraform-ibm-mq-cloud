3209cbdc929db00834d98e6d03c65e18efb9fbad
valerio-bontempi <valerio.bontempi@it.ibm.com>
2025-06-19 15:05:32 +0200

    feat: added upgrade test

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -155,3 +155,45 @@ func TestRunStandardSolutionSchematics(t *testing.T) {
 	err := options.RunSchematicTest()
 	assert.Nil(t, err, "This should not have errored")
 }
+
+func TestRunStandardSolutionUpgradeSchematics(t *testing.T) {
+	// t.Parallel()
+
+	options := testschematic.TestSchematicOptionsDefault(&testschematic.TestSchematicOptions{
+		Testing: t,
+		TarIncludePatterns: []string{
+			"*.tf",
+			"modules/*/*.tf",
+			"modules/*/*.sh",
+			standardSolutionTerraformDir + "/*.tf",
+		},
+		TemplateFolder:         standardSolutionTerraformDir,
+		Tags:                   []string{"test-schematic"},
+		Prefix:                 "mqupg-da",
+		DeleteWorkspaceOnFail:  false,
+		WaitJobCompleteMinutes: 60,
+		Region:                 "us-south",
+	})
+
+	options.TerraformVars = []testschematic.TestSchematicTerraformVar{
+		{Name: "ibmcloud_api_key", Value: options.RequiredEnvironmentVars["TF_VAR_ibmcloud_api_key"], DataType: "string", Secure: true},
+		{Name: "existing_mq_capacity_crn", Value: permanentResources["mq_capacity_crn"], DataType: "string"},
+		{Name: "existing_resource_group_name", Value: resourceGroup, DataType: "string"},
+		{Name: "prefix", Value: options.Prefix, DataType: "string"},
+		{Name: "region", Value: "us-east", DataType: "string"},
+		{Name: "deployment_name", Value: "daupg-mq-instance", DataType: "string"},
+		{Name: "queue_manager_name", Value: "daupg_qm", DataType: "string"},
+		{Name: "queue_manager_display_name", Value: "daupg-qm-display", DataType: "string"},
+		{Name: "queue_manager_size", Value: "xsmall", DataType: "string"},
+		{Name: "application_name", Value: "daupgapp", DataType: "string"},
+		{Name: "user_email", Value: "mqda-user@exmaple.com", DataType: "string"},
+		{Name: "user_name", Value: "mqda-user", DataType: "string"},
+		// forcing provider visibility to public due to this provider bug https://github.ibm.com/GoldenEye/issues/issues/14309
+		{Name: "provider_visibility", Value: "public", DataType: "string"},
+	}
+
+	err := options.RunSchematicUpgradeTest()
+	if !options.UpgradeTestSkipped {
+		assert.NoError(t, err, "Schematic Upgrade Test had an unexpected error")
+	}
+}
