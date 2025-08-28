238ed96f5b01531c3177331b3176e0fe13022640
aatreyee257 <mukherjeeaatreyee25@gmail.com>
2025-08-28 18:04:02 +0530

    fixes

--- outputs.tf
+++ outputs.tf
@@ -3,22 +3,22 @@
 ########################################################################################################################
 
 output "service_instance_capacity_crn" {
-  description = "The CRN of the MQ on Cloud capacity service instance"
+  description = "The CRN of the MQ on Cloud capacity service instance."
   value       = local.capacity_crn
 }
 
 output "service_instance_capacity_guid" {
-  description = "The GUID of the MQ on Cloud capacity service instance"
+  description = "The GUID of the MQ on Cloud capacity service instance."
   value       = local.capacity_guid
 }
 
 output "service_instance_deployment_crn" {
-  description = "The CRN of the MQ on Cloud deployment service instance"
+  description = "The CRN of the MQ on Cloud deployment service instance."
   value       = local.deployment_crn
 }
 
 output "service_instance_deployment_guid" {
-  description = "The GUID of the MQ on Cloud deployment service instance"
+  description = "The GUID of the MQ on Cloud deployment service instance."
   value       = local.deployment_guid
 }
 
@@ -29,5 +29,5 @@ output "queue_manager_id" {
 
 output "queue_manager_options" {
   value       = module.mqcloud_instance.queue_manager_options
-  description = "Queue manager options for the MQ on Cloud deployment service instance"
+  description = "Queue manager options for the MQ on Cloud deployment service instance."
 }
