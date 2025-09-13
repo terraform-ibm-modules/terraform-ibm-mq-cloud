6d9948c60ea274036e6cfb0d8f26861a5d4b46ac
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-09-13 04:15:29 +0100

    fix(deps): update dependencies (#165)

--- solutions/fully-configurable/main.tf
+++ solutions/fully-configurable/main.tf
@@ -206,7 +206,7 @@ locals {
 module "certificate_secret" {
   count                   = var.existing_secrets_manager_crn != null ? 1 : 0
   source                  = "terraform-ibm-modules/secrets-manager-secret/ibm"
-  version                 = "1.8.0"
+  version                 = "1.9.0"
   region                  = module.sm_crn[0].region
   secrets_manager_guid    = module.sm_crn[0].service_instance
   secret_group_id         = local.secret_group_id
@@ -223,7 +223,7 @@ module "certificate_secret" {
 module "root_certificate_secret" {
   count                   = var.existing_secrets_manager_crn != null ? 1 : 0
   source                  = "terraform-ibm-modules/secrets-manager-secret/ibm"
-  version                 = "1.8.0"
+  version                 = "1.9.0"
   region                  = module.sm_crn[0].region
   secrets_manager_guid    = module.sm_crn[0].service_instance
   secret_group_id         = local.secret_group_id
